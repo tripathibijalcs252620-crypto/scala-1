f5df2023a5780b03245e0b01a7bc35aceed501fa
tripathibijalcs252620-crypto <tripathibijalcs252620@gmail.com>
2026-06-23 10:37:04 +0530

    Add files via upload

---
+++ Main.scala
@@ -0,0 +1,8 @@
+
+object WelcomeDataScientists {
+  def main(args : Array[String]): Unit = {
+    println("Welcome, Data Scientists!")
+    println("Lets explore data and build amazing insights.")
+  }
+}
+
